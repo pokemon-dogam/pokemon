06c2bfef0ba3592d7cdd2e73fd7e11bfd12db0d8
Hayden <anwjr7878@gmail.com>
2023-03-05 05:30:26 +0900

    feat: pretierr

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  semi: true,
+  useTabs: false,
+  bracketSameLine: false,
+  proseWrap: 'never',
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: true,
+};
